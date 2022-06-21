6e171122d2fd1e4e06e304ab09fd34f3f4dbdada
Minino99 <loizzo.pasqualino@gmail.com>
2022-06-21 16:09:47 +0200

    navbar done

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
-<div>hello</div>
+  <div>
+    <div class="container">
+      <TheNavbar />
+    </div>
+  </div>
 </template>
 
 <script>
+import TheNavbar from "./components/TheNavbar.vue";
 
 export default {
-  name: 'App',
-  components: {}
-}
+  name: "App",
+  components: { TheNavbar },
+};
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Asap+Condensed:wght@400;700&display=swap");
+
+* {
+  font-family: "Asap Condensed", sans-serif;
+}
 </style>
--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,12 +1,107 @@
 <template>
-  <div></div>
+  <div>
+    <nav class="navbar navbar-light justify-content-between align-items-center">
+      <div class="navbar-brand">
+        <a><img src="../assets/img/dc-logo.png" alt="DC Logo" srcset="" /></a>
+      </div>
+      <div class="d-flex gap-4 navelements">
+        <div
+          class="dcnavelement"
+          v-for="navelement in navElements"
+          :key="navelement.name"
+        >
+          <a :href="navelement.link">{{ navelement.name }}</a>
+        </div>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'TheNavbar',
-  components: {}
-}
+  name: "TheNavbar",
+  data() {
+    return {
+      navElements: [
+        {
+          name: "CHARACTERS",
+          link: "#",
+        },
+        {
+          name: "COMICS",
+          link: "#",
+        },
+        {
+          name: "MOVIES",
+          link: "#",
+        },
+        {
+          name: "TV",
+          link: "#",
+        },
+        {
+          name: "GAMES",
+          link: "#",
+        },
+        {
+          name: "COLLECTIBLES",
+          link: "#",
+        },
+        {
+          name: "VIDEOS",
+          link: "#",
+        },
+        {
+          name: "FANS",
+          link: "#",
+        },
+        {
+          name: "NEWS",
+          link: "#",
+        },
+        {
+          name: "SHOP",
+          link: "#",
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style></style>
+<style lang="scss">
+.navbar {
+  height: 100px;
+  .navbar-brand {
+    height: 100%;
+  }
+  img {
+    height: 100%;
+  }
+}
+
+.navelements {
+  height: 100%;
+
+  .dcnavelement {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    border-bottom: solid 5px transparent;
+    font-size: 0.9rem;
+
+    &:hover {
+      border-bottom: solid 5px #0081ef;
+    }
+
+    &:hover a{
+      color: #0081ef;
+    }
+
+    a{
+     color: black;
+      text-decoration: none;
+    }
+  }
+}
+</style>
